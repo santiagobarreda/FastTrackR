e8fef1c2d568b052d42dcd32b54bf72ed69269ea
Santiago Barreda <sbarreda@ucdavis.edu>
2021-05-04 09:41:21 -0700

    man files

--- R/spectrogramplots.R
+++ R/spectrogramplots.R
@@ -11,39 +11,93 @@
 #' @param main --.
 #' @param colors --.
 #' @param add if FALSE, a new plot if created.
+#' @param spect a spectrogram to be shown behind the tracks.
 #' @param ... Additional arguments are passed to the internal call of 'plot'.
 #' @export
 #' @examples
 #' \dontrun{
 #' sound = tuneR::readWave("yoursound.wav")
 #' ffs = analyze (sound)
+#' spect = spectrogram (sound)
 #' plotffs (ffs)
+#' plotffs (ffs, spect = spect)
 #' plotffs (ffs[[2]])
+#' plotffs (ffs[[2]], spect = spect)
 #' }
 
 plotffs = function(ffs,xlim=NA,ylim=NA,xlab=NA,ylab=NA,
-                   main=NA,colors=NA,add=FALSE,...){
+                   main=NA,colors=NA,add=FALSE,spect=NA,...){
+
+  zcolors = grDevices::colorRampPalette(c("white", "black"))
+  zcolors = zcolors(40)
 
   if (class (ffs)=="fasttrack"){
-    if (attr (ffs,"object")=="ffs")
-      plotffs.internal(ffs,xlim=xlim,ylim=ylim,xlab=xlab,ylab=ylab,
-                       main=main,colors=colors,add=FALSE, ...)
+    if (attr (ffs,"object")=="ffs"){
 
+      if (class(spect)=="fasttrack"){
+        graphics::image (as.numeric(rownames(spect)), as.numeric(colnames(spect)),
+                         spect,col = zcolors,
+                         xlab = "Time (ms)", ylab = "Frequency",...)
+        add = TRUE
+      }
+      plotffs.internal(ffs,xlim=xlim,ylim=ylim,xlab=xlab,ylab=ylab,
+                       main=main,colors=colors,add=add, ...)
+    }
     if (attr (ffs,"object")=="fileffs"){
       tmp_par = graphics::par(no.readonly = TRUE)
 
       n = length (ffs)
       rows = floor(n/4)
 
-      graphics::par (mfrow = c(4,rows), mar =c(.1,.1,2,.1), oma = c(1,1,0,1))
+      graphics::par (mfrow = c(4,rows), mar =c(1,1,2,1), oma = c(1,1,0,1))
       for (i in 1:n){
-        plotffs.internal (ffs[[i]],xaxt='n',yaxt='n', ...)
+        if (class(spect)=="fasttrack"){
+          graphics::image (as.numeric(rownames(spect)), as.numeric(colnames(spect)),
+                           spect,col = zcolors,
+                           xlab = "Time (ms)", ylab = "Frequency")
+          add = TRUE
+        }
+        plotffs.internal (ffs[[i]],xaxt='n',yaxt='n', add = add, ...)
       }
       graphics::par (tmp_par)
     }
   }
-
 }
+
+
+plotffs.internal = function(ffs,xlim=NA,ylim=NA,xlab=NA,ylab=NA,
+                            main=NA,colors=NA,add=FALSE,...){
+
+  if (length(colors)==1) colors = c("darkgoldenrod2",3,4,2)
+  if (is.na(xlab)) xlab="Time (ms)"
+  if (is.na(ylab)) ylab = "Frequency (Hz)"
+  if (is.na(main)) main = paste0 ("maximum formant = ",
+                                  attr(ffs,"maxformant"), " (Hz)")
+
+  timestep = attr (ffs,"timestep")*1000
+
+  n = nrow (ffs)
+  nf = ncol (ffs)/2
+  if (is.na(ylim)) ylim =c(100,max (ffs[,1:nf]))
+  time = 25 + timestep*(0:(n-1))
+  if (is.na(xlim)) xlim = range((time))
+
+
+  if (!add) plot (0, type="n", xlim=xlim, ylim=ylim, xlab=xlab,
+                  ylab=ylab,main=main, ...)
+
+  graphics::points (time, ffs[,1], pch=16, col = colors[1])
+  graphics::lines (time, ffs[,1], col = colors[1])
+  graphics::points (time, ffs[,2], pch=16, col = colors[2])
+  graphics::lines (time, ffs[,2], col = colors[2])
+  graphics::points (time, ffs[,3], pch=16, col = colors[3])
+  graphics::lines (time, ffs[,3], col = colors[3])
+  if (nf==4){
+    graphics::points (time, ffs[,4], pch=16, col = colors[4])
+    graphics::lines (time, ffs[,4], col = colors[4])
+  }
+}
+
 
 
 #' Spectrogram
@@ -61,18 +115,21 @@ plotffs = function(ffs,xlim=NA,ylim=NA,xlab=NA,ylab=NA,
 #' \dontrun{
 #' sound = tuneR::readWave("yoursound.wav")
 #' spectrogram (sound, maxformant = 5000)
+#' ffs = analyze (sound)
+#' plotffs (ffs[[2]], add = TRUE)
 #' }
 
-spectrogram = function (sound, maxformant = 5000, windowlength = 0.009, timestep = 0.005,
+spectrogram = function (sound, maxformant = 5000, windowlength = 0.006, timestep = 0.005,
                         dynamicrange = 60, plot = TRUE, ...){
 
   if (class(sound)=="Wave"){
     fs = sound@samp.rate
+    if (maxformant*2 < fs)
+      sound = tuneR::downsample (sound, maxformant*2)
     tmp_snd = sound@left
   }
+
   tmp_snd = signal::filter (.97,1, tmp_snd)
-  if (maxformant*2 < fs)
-    tmp_snd = tuneR::downsample (sound, maxformant*2)
 
   fs = maxformant*2
   n = length (tmp_snd)
@@ -111,40 +168,6 @@ spectrogram = function (sound, maxformant = 5000, windowlength = 0.009, timestep
   attr(spect, "object") = "spectrogram"
 
   invisible (spect)
-}
-
-
-
-plotffs.internal = function(ffs,xlim=NA,ylim=NA,xlab=NA,ylab=NA,
-                            main=NA,colors=NA,add=FALSE,...){
-
-  if (length(colors)==1) colors = c("darkgoldenrod2",3,4,2)
-  if (is.na(xlab)) xlab="Time (ms)"
-  if (is.na(ylab)) ylab = "Frequency (Hz)"
-  if (is.na(main)) main = paste0 ("maximum formant = ",
-                                    attr(ffs,"maxformant"), " (Hz)")
-
-  n = nrow (ffs)
-  nf = ncol (ffs)/2
-  if (is.na(ylim)) ylim =c(100,max (ffs[,1:nf]))
-  time = 25 + 2*(0:(n-1))
-  if (is.na(xlim)) xlim = range((time))
-
-
-  if (!add)
-    plot (0, type="n", xlim=xlim, ylim=ylim, xlab=xlab,
-          ylab=ylab,main=main, ...)
-
-  graphics::points (time, ffs[,1], pch=16, col = colors[1])
-  graphics::lines (time, ffs[,1], col = colors[1])
-  graphics::points (time, ffs[,2], pch=16, col = colors[2])
-  graphics::lines (time, ffs[,2], col = colors[2])
-  graphics::points (time, ffs[,3], pch=16, col = colors[3])
-  graphics::lines (time, ffs[,3], col = colors[3])
-  if (nf==4){
-    graphics::points (time, ffs[,4], pch=16, col = colors[4])
-    graphics::lines (time, ffs[,4], col = colors[4])
-  }
 }
 
 
