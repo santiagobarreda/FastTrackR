864132a0a9a348664bab5bc544f891db96259a0b
Santiago Barreda <sbarreda@ucdavis.edu>
2021-05-08 10:21:46 -0700

    Praat functionality working

--- R/autoselectwinners.R
+++ R/autoselectwinners.R
@@ -4,17 +4,20 @@
 #'
 #' @param formants a list of formant data read in with the readformants function.
 #' @param order the order of the prediction model.
+#' @param nf the number of formants to optimize for.
 #' @param method method of selecting the winning analysis.
+#' @param outputpath --.
 #' @param subset a vector indicating a subset of the analyses to be considered.
 #' @return A vector with the winning analysis for each file.
 #' @export
 #' @examples
 #' \dontrun{
 #' formants = readformants ()
-#' autoselectwinners (formants)
+#' winners = autoselectwinners (formants, outputpath="working")
 #' }
 
-autoselectwinners <- function (formants, order = 5, method = "classic", subset = NA){
+autoselectwinners <- function (formants, order = 5, nf = 4, method = "classic",
+                               outputpath = NA, subset = NA){
 
   n = length (formants)
   nsteps = length (formants[[1]])
@@ -22,6 +25,8 @@ autoselectwinners <- function (formants, order = 5, method = "classic", subset =
 
   steps = 1:nsteps
   if (!is.na(subset)) steps = subset
+
+  files = sapply (formants, attr, "filename")
 
   errors = matrix (0, n, nsteps)
   for (i in 1:n){
@@ -35,10 +40,19 @@ autoselectwinners <- function (formants, order = 5, method = "classic", subset =
     }
   }
 
-  ## output needs to be the same as in Praat output
   winners = apply (errors, 1, which.min)
   winners = steps[winners]
-  winners
+
+  output = data.frame (file = files, winner = winners, F1=winners,
+                       F2=winners, F3=winners)
+  if (nf==4) output[["F4"]] = winners
+
+  if (!is.na (outputpath)){
+    if (outputpath == "working") outputpath = getwd()
+    utils::write.csv (output, outputpath %+% "/winners.csv", row.names=FALSE)
+  }
+
+  output
 }
 
 
@@ -46,4 +60,3 @@ makepredictors = function (n, order){
   x = (0:(n-1)) / n
   xs = sapply (seq (0,order/2,.5), function (f) cos (f*2*pi*x))
 }
-
