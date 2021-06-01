bf2994f8a357374cb104c415f4f930d62bbb173e
Santiago Barreda <sbarreda@ucdavis.edu>
2021-06-01 13:13:41 -0700

    Create outliers.lda.R

---
+++ R/outliers.lda.R
@@ -0,0 +1,49 @@
+
+#' Find outliers using Linear Discriminant Analysis 
+#'
+#' 
+#'
+#' @param aggregated_data --.
+#' @return --.
+#' @export
+#' @examples
+#' \dontrun{
+#'  # coming soon
+#' }
+
+ourliers.lda <- function (aggregated_data){
+  
+  colnms = colnames(aggregated_data)
+  start = which(colnms=="f11")
+  colnms = colnms[nchar (colnms) == 3]
+  nf = substr (colnms,2,2)
+  nf = suppressWarnings(nf[!is.na(as.numeric(nf))])
+  nf = max (as.numeric (nf))
+  nt = substr (colnms,3,3)
+  nt = suppressWarnings(nt[!is.na(as.numeric(nt))])
+  nt = max (as.numeric (nt))
+  end = start+nf*nt-1
+
+  ffs = aggregated_data[,which(colnames(aggregated_data)=="f11"):ncol(aggregated_data)]
+  ffs = log (as.matrix (ffs))
+  
+  form = label ~ ffs
+  #ldamod = MASS::lda (aggregated_data$label ~ ffs)
+  ldamod = MASS::lda (aggregated_data$label ~ ffs, method = 't')
+  #ldamod
+  
+  ldapred = predict (ldamod)
+  
+  file = aggregated_data$file
+  label = as.numeric(ldapred$class == aggregated_data$label)
+  winner = ldapred$class
+  posterior = apply (ldapred$posterior,1,max)
+  posterior = round (posterior, 4)
+  distance = abs(log(ldapred$posterior))
+  distance = round (distance, 3)
+  
+  output = data.frame (file, label,winner,correct,posterior)
+  output$distance = distance
+  output
+}
+
