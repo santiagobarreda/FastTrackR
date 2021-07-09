a8e5343a5d5d3cabefe04e931d3336e7cabc68bd
Santiago Barreda <sbarreda@ucdavis.edu>
2021-07-09 05:56:34 -0700

    Update autoselect.write.R

--- R/autoselect.write.R
+++ R/autoselect.write.R
@@ -7,10 +7,12 @@ autoselect.write <- function (outputpath, output){
   coefficients = output$coefficients  
   
   utils::write.csv (winners_csv, outputpath %+% "/winners.csv", row.names=FALSE)
+  utils::write.csv (winners_csv, outputpath %+% "/winners_backup.csv", row.names=FALSE)
   
   colnames(total_errors) = paste0("e",1:ncol(total_errors))
   colnames(coefficients) = paste0("a",1:dim(coefficients)[2])
   dir.create (outputpath %+% "/infos_aggregated/", showWarnings = FALSE)
+  dir.create (outputpath %+% "/regression_infos/", showWarnings = FALSE)
   
   utils::write.csv (total_errors, outputpath %+% "/infos_aggregated/all_errors.csv", row.names=FALSE)
   utils::write.csv (coefficients[,,1,1], outputpath %+% "/infos_aggregated/all_f1s.csv", row.names=FALSE)
@@ -20,20 +22,49 @@ autoselect.write <- function (outputpath, output){
   nf = dim(errors)[3]
   
   info_files = list.files (outputpath %+% "/infos/")
+  reg_files = tools::file_path_sans_ext( list.files (outputpath %+% "/sounds/"))
+  reg_files = paste0 (reg_files,".txt")
+  
   for (i in 1:length (info_files)){
-    tmp_info  = readLines (outputpath %+% "/infos/" %+% info_files[i])
+    tmp_info  = readLines (outputpath %+% "/infos/" %+% info_files[i])[1:9]
+    
+    tmp_info[10] = "Winner is:"
     tmp_info[11] = winners_csv[i,2]
     
     errors_out = t(cbind(total_errors[i,],errors[i,,1:nf]))
     errors_out = apply (errors_out, 1, paste, collapse = " ")
-    tmp_info[13:(12+length(errors_out))] = errors_out
+    if(nf==3) tmp_info[12] = "Errors (total,f1,f2,f3):"
+    if(nf==4) tmp_info[12] = "Errors (total,f1,f2,f3,f4):"
+    tmp_info = c(tmp_info, errors_out)
     
     coeffs_out = coefficients[i,winners_csv[i,2],,]
     coeffs_out = apply (coeffs_out, 1, paste, collapse = " ")
     
-    spot=which(tmp_info=="Coefficients are (row-wise by formant):")
-    tmp_info = c(tmp_info[1:spot],coeffs_out)
+    tmp_info = c(tmp_info,"Coefficients are (row-wise by formant):")
+    tmp_info = c(tmp_info,coeffs_out)
     
     writeLines (tmp_info, outputpath %+% "/infos/" %+% info_files[i])
+
+    # writeLines (tmp_info, outputpath %+% "/regression_infos/" %+% info_files[i])
+    
+    #reg_files
+    #base = tools::file_path_sans_ext(reg_files[i])
+    
+    # regression information text file output coming soon.
+    # tmp_reg = list("Regression analysis information for: " %+% base,
+    #             "number of formants: " %+% nf,
+    #             "number of coefficients: " %+% tmp_info[7])
+    # count = 4
+    # for (j in 1:as.numeric(tmp_info[3])){
+    #   tmp_reg[[count]] = paste (errors[i,j,], collapse =" ")
+    #   count = count + 1
+    #   tmp_reg[[count]] = t(coefficients[i,j,,])
+    #   count = count + 1
+    # }
+    # write(tmp_reg[[1]],"test2.txt", append=FALSE)
+    # lapply(tmp_reg[-1], write, "test2.txt", append=TRUE)
   }
 }
+
+
+
