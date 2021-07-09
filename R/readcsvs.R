c62eca21c809d6bf49d2652211b6be31d3faafbd
Santiago Barreda <sbarreda@ucdavis.edu>
2021-07-09 14:56:32 -0700

    faster read in

--- R/readcsvs.R
+++ R/readcsvs.R
@@ -7,6 +7,7 @@
 #'
 #' @param path The path to the working directory for the Fast Track project. If no path is provided, the current working directory for the current R session is used.
 #' @param asone If TRUE (the default), the csv files are combined into one dataframe and filenames are indicated in a new column. If FALSE, a list of dataframes is returned and each list element is named after the file.
+#' @param progressbar --
 #' @return A dataframe or list of dataframes, as determined by the \code{asone} parameter.
 #' @export
 #' @examples
@@ -14,19 +15,20 @@
 #' csvs <- readcsvs()
 #' }
 
-readcsvs <- function (path, asone = TRUE){
+readcsvs <- function (path, asone = TRUE, progressbar = FALSE){
   
   if (missing(path)) path = getwd()
   files = list.files (paste0(path,"/csvs"),full.names=TRUE)
   file_names = list.files (paste0(path,"/csvs"))
   file_names = substr (file_names,1, nchar(file_names)-4)
 
-  csvs = list()
-  for (i in 1:length(files)){
+  n_files = length(files)
+  csvs = lapply (1:n_files, function(i){
+    if (progressbar) progressbar(i,n_files)
     csvs[[i]] = utils::read.csv (files[i], na.strings = "0")
     if (!asone) names (csvs)[i] = file_names[i]
     if (asone) csvs[[i]]$file = file_names[i]
-  }
+  })
   if (asone) csvs = do.call (rbind, csvs)
 
   attr(csvs, "object") = "csvs"
