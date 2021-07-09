3f093e946103143be4dc6a333d1d8ecbaa3b3d89
Santiago Barreda <sbarreda@ucdavis.edu>
2021-07-09 14:56:18 -0700

    Update makewinnerplots.R

--- R/makewinnerplots.R
+++ R/makewinnerplots.R
@@ -2,25 +2,31 @@
 #' Make images for winning analysies
 #'
 #' @param path --.
-#' @param resolution --.
 #' @param csvs --.
+#' @param height --.
+#' @param width --.
+#' @param pointsize --.
+#' @param number_of_lines --.
 #' @param showprogressbar --.
 #' @export
 #' @examples
 #' \dontrun{
 #' # nothing yet
 #' system.time (makewinnerplots(showprogressbar=TRUE))
+#' makewinnerplots(csvs = csvs[1:20])
 #' }
 
-makewinnerplots <- function (path, resolution = 1000, csvs = NA, showprogressbar = FALSE){
+makewinnerplots <- function (path=NA, csvs = NA, height=1000, width = 1400, 
+                             pointsize = 20, number_of_lines = 250, 
+                             showprogressbar = TRUE){
 
-  if (missing (path)) path = getwd()
+  if (is.na(path)) path = getwd()
 
   csv_files = list.files (path %+% "/csvs")
 
-  if (is.na(csvs) & length(csv_files)==0)
+  if (is.na(csvs[1]) & length(csv_files)==0)
     stop ("No csvs available or provided.")
-  if (is.na(csvs) & length(csv_files)>0)
+  if (is.na(csvs[1]) & length(csv_files)>0)
     csvs = readcsvs (path, asone = FALSE)
 
   dir.create(path %+% "/images_winners", showWarnings = FALSE)
@@ -28,13 +34,16 @@ makewinnerplots <- function (path, resolution = 1000, csvs = NA, showprogressbar
   filenames = names (csvs)
 
   for (i in 1:length (filenames)){
+    
     filename = path %+% "/images_winners/" %+% filenames[i] %+% ".png"
     grDevices::png (filename, height = 1000, width = 1400, pointsize = 24)
-
+  
+    graphics::par (mar =c(4,4,2,1))
     sound = tuneR::readWave (path %+% "/sounds/" %+% filenames[i] %+% ".wav")
     # spectrogram resolution should relate to image resolution
-    spect = spectrogram (sound, plot = FALSE)
-    plotffs (csvs[[i]], spect = spect, main = "")
+    spect = spectrogram (sound, plot = FALSE, resolution = 50, timestep = number_of_lines)
+    plotffs (csvs[[i]], spect = spect, lwd=3,pch=16,cex=1.25)
+    graphics::title (filenames[i], cex = 0.9)
 
     grDevices::dev.off()
     if (showprogressbar) progressbar(i, length (filenames))
