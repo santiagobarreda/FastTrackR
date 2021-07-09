2537869b4878c462f383082b1a577ba57de1b707
Santiago Barreda <sbarreda@ucdavis.edu>
2021-07-09 12:19:52 -0700

    Update plotffs.R

--- R/plotffs.R
+++ R/plotffs.R
@@ -4,6 +4,7 @@
 #' Set up a plot aggregated files. This function does not draw anything but is used to set up the plot for other functions.
 #'
 #' @param ffs a dataframe containing formant tracks for a single file, or a list of dataframes comparing multiple analyses for a single file.
+#' @param winner an optional dataframe representing the winners file.
 #' @param xlim an optional user-specified x-axis range.
 #' @param ylim an optional user-specified x-axis range.
 #' @param xlab an optional user-specified x-axis label.
@@ -28,7 +29,7 @@
 #' plotffs (ffs, spect = spect)
 #' }
 
-plotffs = function(ffs,xlim=NA,ylim=NA,xlab=NA,ylab=NA,
+plotffs = function(ffs,winner=NA,xlim=NA,ylim=NA,xlab=NA,ylab=NA,
                    main=NA,colors=NA,cex=NA,lwd=NA,pch=NA,add=FALSE,spect=NA,...){
 
   zcolors = grDevices::colorRampPalette(c("white", "black"))
@@ -52,17 +53,20 @@ plotffs = function(ffs,xlim=NA,ylim=NA,xlab=NA,ylab=NA,
     n = length (ffs)
     rows = floor(n/4)
 
-    graphics::par (mfrow = c(4,rows), mar =c(1,1,2,1), oma = c(1,1,0,1))
+    graphics::par (mfrow = c(4,rows), mar =c(1.5,1,2,1), oma = c(1,1,0,1))
     for (i in 1:n){
+      main = paste0 ("Maximum formant = ",attr(ffs[[i]], "maxformant")," Hz")
       if (!is.na(spect[1])){
         if (attr(spect, "object")=="spectrogram"){
           graphics::image (as.numeric(rownames(spect)), as.numeric(colnames(spect)),
                            spect,col = zcolors,
-                           xlab = "Time (ms)", ylab = "Frequency")
+                           xlab = "Time (ms)", ylab = "Frequency", main=main)
           add = TRUE
         }
       }
-      plotffs.internal (ffs[[i]],xaxt='n',yaxt='n', add = add, ...)
+      plotffs.internal (ffs[[i]],xaxt='n',yaxt='n', add = add, main = main,
+                        colors=colors,cex=cex,lwd=lwd,pch=pch, ...)
+      if(!is.na(winner)) if (winner==i) graphics::box(lwd=3, col = 2)
     }
     graphics::par (tmp_par)
   }
