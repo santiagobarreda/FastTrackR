9ce8ca66debc864f56bdde3612ef2bd480fe05a2
Santiago Barreda <sbarreda@ucdavis.edu>
2021-08-14 14:20:25 -0700

    added single file option

--- R/makewinnerplots.R
+++ R/makewinnerplots.R
@@ -1,7 +1,7 @@
 
 #' Make images for winning analysies
 #'
-#' @param path --.
+#' @param path The path to the working directory for the Fast Track project. If no path is provided, the current working directory for the current R session is used.
 #' @param csvs --.
 #' @param height --.
 #' @param width --.
@@ -33,20 +33,30 @@ makewinnerplots <- function (path=NA, csvs = NA, height=1000, width = 1400,
 
   filenames = names (csvs)
 
-  for (i in 1:length (filenames)){
+  if (length(filenames)>1){
+    for (i in 1:length (filenames)){
+      
+      filename = path %+% "/images_winners/" %+% filenames[i] %+% ".png"
+      grDevices::png (filename, height = 1000, width = 1400, pointsize = 24)
     
-    filename = path %+% "/images_winners/" %+% filenames[i] %+% ".png"
-    grDevices::png (filename, height = 1000, width = 1400, pointsize = 24)
+      graphics::par (mar =c(4,4,2,1))
+      sound = tuneR::readWave (path %+% "/sounds/" %+% filenames[i] %+% ".wav")
+      # spectrogram resolution should relate to image resolution
+      spect = spectrogram (sound, plot = FALSE, resolution = 50, timestep = number_of_lines)
+      plotffs (csvs[[i]], spect = spect, lwd=3,pch=16,cex=1.25)
+      graphics::title (filenames[i], cex = 0.9)
   
+      grDevices::dev.off()
+      if (showprogressbar) progressbar(i, length (filenames))
+    }
+  }
+  if (length(filenames)==1){
     graphics::par (mar =c(4,4,2,1))
-    sound = tuneR::readWave (path %+% "/sounds/" %+% filenames[i] %+% ".wav")
+    sound = tuneR::readWave (path %+% "/sounds/" %+% filenames %+% ".wav")
     # spectrogram resolution should relate to image resolution
     spect = spectrogram (sound, plot = FALSE, resolution = 50, timestep = number_of_lines)
-    plotffs (csvs[[i]], spect = spect, lwd=3,pch=16,cex=1.25)
-    graphics::title (filenames[i], cex = 0.9)
-
-    grDevices::dev.off()
-    if (showprogressbar) progressbar(i, length (filenames))
+    plotffs (csvs, spect = spect, lwd=3,pch=16,cex=1.25)
+    graphics::title (filenames, cex = 0.9)
   }
 }
 
