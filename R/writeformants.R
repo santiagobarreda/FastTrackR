1af5f73de8ae2650a1a41f0b93c28de7152fe665
Santiago Barreda <sbarreda@ucdavis.edu>
2021-08-26 12:42:33 -0700

    done. writes Praat-usable formant files

--- R/writeformants.R
+++ R/writeformants.R
@@ -3,56 +3,43 @@
 #' @param formants --.
 #' @param path --.
 #' @export
-writeformants <- function (formants, path){
+writeformants <- function (formants, path = NA){
 
-  snd_files = list.files (paste0(path,"/sounds"),full.names=TRUE)
-  formant_files = list.files (paste0(path,"/formants"),full.names=TRUE)
-  time_step = readLines (formant_files)[7]
-
-  tmp_formants = formants[[1]][[1]]
-  nf = as.character(ncol(tmp_formants)/2)
-  n_analyses = length(formants[[1]])
-
-  file_base = unlist (strsplit (formant_files, split="_"))
-  file_base = matrix (file_base, ncol = 3, byrow=TRUE)[,1]
-  file_base = unique (file_base)
+  if (is.na (path)) path = getwd()
 
   header = c("File type = \"ooTextFile\"",
              "Object class = \"Formant 2\"",
              "",
              "0",
-             duration=0,
+             "duration",
              0,
-             time_step,
+             "time_step",
              0.025, # first analysis
-             nf) # maximum number of formants
+             "nf") # maximum number of formants
+  
+  n_analyses = attr(formants,"ncutoffs")
 
-
-  for (i in 1:length(snd_files)){
-    tmp_snd = readWave2 (snd_files[i])
-    duration = length(tmp_snd@left)/tmp_snd@samp.rate
-    header[5] = duration
-
+  for (i in 1:length(formants)){
+    print (i)
     for (j in 1:n_analyses){
       tmp_formants = formants[[i]][[j]]
+
+      filename = attr(tmp_formants,"filename")
+      nf = ncol(tmp_formants)/2
+
+      header[5] = attr(tmp_formants,"duration")
       header[6] = nrow(tmp_formants)
+      header[7] = attr(tmp_formants,"timestep")
+      header[9] = nf
 
       tmp_out = c(rbind(.2, nf, t(tmp_formants)))
       tmp_out = c(header, tmp_out)
-      filename = file_base[i] %+% "_" %+% j %+% "_" %+% ".Formant"
-      writeLines (tmp_out, filename)
+      output_file = filename %+% "_" %+% j %+% "_" %+% ".Formant"
+      
+      writeLines (tmp_out, paste0 (path, "/",output_file))
     }
   }
 }
-
-
-
-
-
-
-
-
-
 
 
 
