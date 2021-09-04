fa6d8584ac31c95c8bb1ab76b0e464d0beb051a6
Santiago Barreda <sbarreda@ucdavis.edu>
2021-09-03 20:16:49 -0700

    Create loading_utils.R

---
+++ R/loading_utils.R
@@ -0,0 +1,67 @@
+
+
+load_file_info = function (path, encoding = NA){
+  
+  fileinfo_exists = FALSE
+  if (is.na(encoding)) encoding = "UTF-8"
+  
+  if (file.exists (path %+% "/file_information.RDS")){
+    fileinfo = readRDS(path %+% "/file_information.RDS")
+    fileinfo_exists = TRUE
+  }
+  
+  if (!fileinfo_exists & file.exists (path %+% "/file_information.csv")){
+    fileinfo = tryCatch({
+      utils::read.csv(path %+% "/file_information.csv",blank.lines.skip=FALSE,
+                      stringsAsFactors=FALSE, encoding = encoding)
+    }, warning = function(warning_condition){
+      stop ("Problem reading in file_information. Please specify an encoding.")
+    }, error = function(error_condition) {
+      stop ("Problem reading in file_information. Please specify an encoding.")
+    })
+    fileinfo_exists = TRUE
+    
+    saveRDS (fileinfo, path %+% "/file_information.RDS")
+  }
+  if (!fileinfo_exists){
+    cat ("No file information exists in your working directory (and none was provided).")
+    cat ("A default one was generated and saved in your working directory.")
+    fileinfo = makefileinformation(path)
+    utils::write.csv (fileinfo, "file_information.csv", row.names = FALSE,quote=FALSE)
+    writeRDS(fileinfo, path %+% "/file_information.RDS")
+  }
+  
+  return (fileinfo)
+}
+
+
+load_segmentation_info = function (path, encoding = NA){
+  
+  segmentationinfo_exists = FALSE
+  if (is.na(encoding)) encoding = "UTF-8"
+  
+  if (file.exists (path %+% "/segmentation_information.RDS")){
+    segmentationinfo = readRDS(path %+% "/segmentation_information.RDS")
+    segmentationinfo_exists = TRUE
+  }
+  
+  if (!segmentationinfo_exists & file.exists (path %+% "/segmentation_information.csv")){
+    segmentationinfo = tryCatch({
+      utils::read.csv(path %+% "/segmentation_information.csv",blank.lines.skip=FALSE,
+                      stringsAsFactors=FALSE, encoding = encoding)
+    }, warning = function(warning_condition){
+      stop ("Problem reading in file_information. Please specify an encoding.")
+    }, error = function(error_condition) {
+      stop ("Problem reading in file_information. Please specify an encoding.")
+    })
+    segmentationinfo_exists = TRUE
+    
+    saveRDS (segmentationinfo, path %+% "/segmentation_information.RDS")
+  }
+  
+  if (segmentationinfo_exists) return (segmentationinfo)
+  if (!segmentationinfo_exists) return (NA)
+}
+
+
+
