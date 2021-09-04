2541d1eb8f81c82e2fa3ef001708f9d417468a0a
Santiago Barreda <sbarreda@ucdavis.edu>
2021-09-03 20:16:52 -0700

    Update makefileinformation.R

--- R/makefileinformation.R
+++ R/makefileinformation.R
@@ -4,10 +4,9 @@
 #' Make a file_information.csv file if none exists.
 #'
 #' @param path the path to the working directory for the Fast Track project. If no path is provided this is the working directory.
-#' @param write if TRUE, the file is written to the indicated path(or working directory).
 #' @export
 
-makefileinformation = function (path = NA, write=TRUE){
+makefileinformation = function (path = NA){
   
   if (is.na (path)) path = getwd()
   
@@ -23,9 +22,6 @@ makefileinformation = function (path = NA, write=TRUE){
   
   fileinformation = data.frame (number = number, file = files,label=label, 
                                 group = group, color = color)
-  
-  if (write)
-    utils::write.csv (fileinformation, "file_information.csv", row.names = FALSE,quote=FALSE)
   
   fileinformation
   
