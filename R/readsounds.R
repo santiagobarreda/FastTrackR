1f6f85ceb6c373daae46fea0fbd6755a563abd81
Santiago Barreda <sbarreda@ucdavis.edu>
2021-10-12 07:18:35 -0700

    Update readsounds.R

--- R/readsounds.R
+++ R/readsounds.R
@@ -4,14 +4,17 @@
 #' This function writes out R internal formant data into text files that can be used in Praat. 
 #' 
 #' @param path the path to the working directory for the Fast Track project. If no path is provided this is the working directory.
+#' @param save --.
 #' @export
 
-readsounds = function (path = NA){
+readsounds = function (path = NA, save = TRUE){
   
   if (is.na (path)) path = getwd()
   filenames = list.files (path %+% "/sounds/", full.names=TRUE)
   sounds = lapply (filenames, readWave2)
   
-  sounds
+  if (save) saveRDS (sounds, "sounds.RDS")
+  
+  invisible (sounds)
 }
 
