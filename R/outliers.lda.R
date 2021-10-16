fcdc69c65f29e71ccbf4772482f4cd6b9a155e07
Santiago Barreda <sbarreda@ucdavis.edu>
2021-10-16 09:30:53 -0700

    Update outliers.lda.R

--- R/outliers.lda.R
+++ R/outliers.lda.R
@@ -27,11 +27,27 @@ outliers.lda <- function (aggregated_data){
   ffs = aggregated_data[,which(colnames(aggregated_data)=="f11"):ncol(aggregated_data)]
   ffs = log (as.matrix (ffs))
   
-  form = label ~ ffs
-  #ldamod = MASS::lda (aggregated_data$label ~ ffs)
-  ldamod = MASS::lda (aggregated_data$label ~ ffs, method = 't')
-  #ldamod
+  label = aggregated_data$label
   
+  means = stats::aggregate (ffs ~ label, FUN = mean)[,-1]
+  idx = as.numeric(factor(label))
+  
+  centered_ffs = as.matrix(ffs - means[idx,])
+  est_cov = stats::cov(centered_ffs)
+  
+  distances = 
+    sapply (1:nrow(ffs), function (j) 
+          sapply (1:nrow (means), 
+                  function (i) stats::mahalanobis(ffs[j,], unlist(means[i,]),est_cov)))
+  distances= t(distances)
+  
+  denom = matrix (rowSums(exp(distances)),nrow(distances),ncol(distances))
+  posteriors = round (exp(distances) / denom, 4)
+  
+  labss = sort(unique(aggregated_data$label))[apply (distances,1,which.min)]
+  
+  
+  ldamod = MASS::lda (aggregated_data$label ~ ffs, method = 'moment')
   ldapred = stats::predict (ldamod)
   
   file = aggregated_data$file
@@ -40,11 +56,14 @@ outliers.lda <- function (aggregated_data){
   winner = ldapred$class
   posterior = apply (ldapred$posterior,1,max)
   posterior = round (posterior, 4)
-  distance = abs(log(ldapred$posterior))
-  distance = round (distance, 3)
-  
-  output = data.frame (file, label,winner,correct,posterior)
-  output$distance = distance
+
+  output = data.frame (file, label,winner,correct,posterior.winner = posterior)
+  if (!distance) output$posterior = round (ldapred$posterior,4)
+  if (distance){
+    distance = abs(log(ldapred$posterior))
+    distance = round (distance, 3)
+    output$distance = distance
+  }
   output
 }
 
