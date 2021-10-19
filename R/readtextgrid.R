dc9bb0fae08a0c01ed51aaab73b9dc83858e9a86
Santiago Barreda <sbarreda@ucdavis.edu>
2021-10-19 05:06:00 -0700

    Update readtextgrid.R

--- R/readtextgrid.R
+++ R/readtextgrid.R
@@ -52,43 +52,45 @@ readtextgrid <- function (path, encoding = "UTF-8"){
   outputs = list ()
   # for each tier
   for (i in 1:length (tiers)){
-    # short format processing
-    if (filetype=="short"){
-      start = (tiers[i]+5)
-      end = (tiers[i]+4+tier_n[i]*3)
-
-      # subsection of vector corresponding to each tier is changed into 
-      # a matrix and cleaed up for output
-      output = matrix (tg[start:end],(end-start+1)/3,3,byrow=TRUE)
-      output = data.frame (output[,c(3,1,2)])
-      colnames(output) = c("label", "start", "end")
-      output$duration = as.numeric(output[,3])-as.numeric(output[,2])
-
-      output[,1] = gsub ("text = ", "", output[,1])
-      output[,1] = gsub (" ", "", output[,1])
-      output[,1] = gsub ("\t", "", output[,1])
-      output[,1] = gsub ("\\\"", "", output[,1])
+    if (tier_n[i] > 1){
+      # short format processing
+      if (filetype=="short"){
+        start = (tiers[i]+5)
+        end = (tiers[i]+4+tier_n[i]*3)
+  
+        # subsection of vector corresponding to each tier is changed into 
+        # a matrix and cleaed up for output
+        output = matrix (tg[start:end],(end-start+1)/3,3,byrow=TRUE)
+        output = data.frame (output[,c(3,1,2)])
+        colnames(output) = c("label", "start", "end")
+        output$duration = as.numeric(output[,3])-as.numeric(output[,2])
+  
+        output[,1] = gsub ("text = ", "", output[,1])
+        output[,1] = gsub (" ", "", output[,1])
+        output[,1] = gsub ("\t", "", output[,1])
+        output[,1] = gsub ("\\\"", "", output[,1])
+      }
+      # long format processing
+      if (filetype=="long"){
+        start = (tiers[i]+5)
+        end = (tiers[i]+4+as.numeric(tier_n[i])*4)
+  
+        # same as above but some more processing related to the extra 
+        # text in the long format
+        output = matrix (tg[start:end],(end-start+1)/4,4,byrow=TRUE)
+        output = data.frame (output[,c(4,2,3)])
+        colnames(output) = c("label", "start", "end")
+        output[,2] = as.numeric(gsub ("xmin = ", "", output[,2]))
+        output[,3] = as.numeric(gsub ("xmax = ", "", output[,3]))
+        output$duration = as.numeric(output[,3])-as.numeric(output[,2])
+  
+        output[,1] = gsub ("text = ", "", output[,1])
+        output[,1] = gsub (" ", "", output[,1])
+        output[,1] = gsub ("\t", "", output[,1])
+        output[,1] = gsub ("\\\"", "", output[,1])
+      }
+      outputs[[i]] = output
     }
-    # long format processing
-    if (filetype=="long"){
-      start = (tiers[i]+5)
-      end = (tiers[i]+4+as.numeric(tier_n[i])*4)
-
-      # same as above but some more processing related to the extra 
-      # text in the long format
-      output = matrix (tg[start:end],(end-start+1)/4,4,byrow=TRUE)
-      output = data.frame (output[,c(4,2,3)])
-      colnames(output) = c("label", "start", "end")
-      output[,2] = as.numeric(gsub ("xmin = ", "", output[,2]))
-      output[,3] = as.numeric(gsub ("xmax = ", "", output[,3]))
-      output$duration = as.numeric(output[,3])-as.numeric(output[,2])
-
-      output[,1] = gsub ("text = ", "", output[,1])
-      output[,1] = gsub (" ", "", output[,1])
-      output[,1] = gsub ("\t", "", output[,1])
-      output[,1] = gsub ("\\\"", "", output[,1])
-    }
-    outputs[[i]] = output
   }
   
   # return a list of dataframes, one for each interval tier. 
